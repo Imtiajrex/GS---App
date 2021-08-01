9df97727bf1fa17ce50fc8bd88a7f5c9d3830993
Imtiaj Shuvo <imtiajrex@gmail.com>
2021-08-01 13:45:00 +0600

    Added Libraries

---
+++ src/navigation/RootNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class RootNav extends Component {
+	render() {
+		return (
+			<View>
+				<Text> Root </Text>
+			</View>
+		);
+	}
+}
